c86b2297622e66da51f231779d6c158e78361bb8
Tristan Deane <tristandeane93@gmail.com>
2024-05-19 18:22:58 -0600

    Hardcode rollbar token

--- server/src/common/logs.ts
+++ server/src/common/logs.ts
@@ -1,7 +1,7 @@
 import Rollbar = require("rollbar");
 
 export const rollbar = new Rollbar({
-	accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+	accessToken: "e7e2e2ef986045dc9578c57a50c2db98",
 	environment: process.env.NODE_ENV || "development",
 	captureUncaught: true,
 	captureUnhandledRejections: true,

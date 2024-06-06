736225c5df3117c866d8fad437a6ae4f80e217bb
Tristan Deane <tristandeane93@gmail.com>
2024-06-06 15:51:58 -0600

    Update settings step

--- client/src/common/commands.ts
+++ client/src/common/commands.ts
@@ -1,11 +1,15 @@
-// commands.ts
 import * as vscode from "vscode";
 import { LanguageClient } from "vscode-languageclient/node";
 import { Credentials, UserSession } from "./auth/github";
 import { signInWithGitHub, signOutUser } from "./auth/api";
-import { COMMANDS, SESSION_USER } from "./constants";
+import { COMMANDS, EXTENSION_ID, EXTENSION_NAME, PUBLISHER, SESSION_USER } from "./constants";
 import { renameSymbolWithSuggestions } from './utils/rename';
 import { trackUserInterestInCustomRules } from "./logs";
+
+const WORKBENCH_ACTIONS = {
+	OPEN_WALKTHROUGH: 'workbench.action.openWalkthrough',
+	OPEN_SETTINGS: 'workbench.action.openSettings'
+};
 
 export function registerCommands(context: vscode.ExtensionContext, client: LanguageClient, deactivate: () => void){
     const credentials = new Credentials();
@@ -32,15 +36,21 @@ export function registerCommands(context: vscode.ExtensionContext, client: Langu
         COMMANDS.ADD_CUSTOM_RULE,
         () => {
 			const storedUser: UserSession = context.globalState.get(SESSION_USER);
-            vscode.window.showInformationMessage('Thanks for your interest in custom rules. This feature is coming soon!');
+            vscode.window.showInformationMessage('Thanks for your interest in automated rules setup. This feature is coming soon! 🚀');
             trackUserInterestInCustomRules(storedUser.id);
         }
     );
     context.subscriptions.push(addCustomRuleCommand);
 
     const openWalkthroughCommand = vscode.commands.registerCommand(
-        'whenInRome.openWalkthrough',
-        () => vscode.commands.executeCommand('workbench.action.openWalkthrough', 'whenInRome.gettingStarted', true)
+        COMMANDS.OPEN_WALKTHROUGH,
+        () => vscode.commands.executeCommand(WORKBENCH_ACTIONS.OPEN_WALKTHROUGH, `${EXTENSION_ID}.gettingStarted`, true)
     );
     context.subscriptions.push(openWalkthroughCommand);
+
+	const openSettingsCommand = vscode.commands.registerCommand(
+        COMMANDS.OPEN_SETTINGS,
+        () => vscode.commands.executeCommand(WORKBENCH_ACTIONS.OPEN_SETTINGS, `@ext:${PUBLISHER}.${EXTENSION_NAME}`)
+    );
+    context.subscriptions.push(openSettingsCommand);
 }

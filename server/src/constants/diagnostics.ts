d26221a93eacd294fea87c969b80289b0a1e7127
Tristan Deane <tristandeane93@gmail.com>
2024-07-21 15:18:54 -0600

    Update diagnostic source types

--- server/src/constants/diagnostics.ts
+++ server/src/constants/diagnostics.ts
@@ -1,6 +1,6 @@
-export const SOURCE_NAME = "whenInRome";
-export const NAMING_CONVENTION_VIOLATION_SOURCE_TYPE = "namingConventionViolation";
-export const THEME_SYSTEM_VIOLATION_SOURCE_TYPE = "themeSystemViolation";
+export const SOURCE_NAME = "when-in-rome";
+export const NAMING_CONVENTION_VIOLATION_SOURCE_TYPE = "naming-convention-violation";
+export const THEME_SYSTEM_VIOLATION_SOURCE_TYPE = "theme-system-violation";
 export const RENAME_SUGGESTION_PLACEHOLDER = "Thanks for your interest in the rename symbol suggestion! We're working on this feature.";
 export const DJANGO_BEST_PRACTICES_VIOLATION_SOURCE_TYPE = "django-best-practices-violation";
 export const DJANGO_NPLUSONE_VIOLATION_SOURCE_TYPE = "django-nplusone-query-violation";

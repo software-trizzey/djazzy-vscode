302beccd4ec7352363fc1bcb89a2a978abc70474
Tristan Deane <tristandeane93@gmail.com>
2024-08-08 12:04:38 -0600

    Remove log file output

--- server/bundled/tools/python/log.py
+++ server/bundled/tools/python/log.py
@@ -1,11 +1,5 @@
 import logging
 
-logging.basicConfig(
-    filename='django_analyzer.log',
-    level=logging.DEBUG, 
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
 LOGGER = logging.getLogger(__name__)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)

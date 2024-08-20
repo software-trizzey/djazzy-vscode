b143706eabfdc14aa892320b4569cad08497e80e
Tristan Deane <tristandeane93@gmail.com>
2024-08-20 13:34:08 -0600

    Improve queryset optimization detection by fixing comparison checks

--- server/bundled/tools/python/nplusone/simple_analyzer.py
+++ server/bundled/tools/python/nplusone/simple_analyzer.py
@@ -8,7 +8,6 @@ from log import LOGGER
 class SimplifiedN1Detector:
     QUERYSET_METHODS = {'filter', 'all', 'get', 'exclude', 'order_by', 'prefetch_related', 'select_related'}
 
-
     def __init__(self, source_code: str):
         self.source_code = source_code
         self.issues: List[Dict[str, Any]] = []
@@ -29,22 +28,51 @@ class SimplifiedN1Detector:
     def analyze_function(self, node: ast.FunctionDef):
         LOGGER.debug("Finding optimized querysets in function: %s", node.name)
         self.find_optimized_querysets(node)
+        LOGGER.debug("Querysets found: %s", self.found_querysets)
         LOGGER.debug("Optimized querysets found: %s", self.optimized_querysets)
         
         loops = self.find_loops(node)
         LOGGER.debug("Found %d loops in function %s", len(loops), node.name)
         for loop in loops:
             self.analyze_loop(node, loop)
+        
+        self.found_querysets.clear()
+        self.optimized_querysets.clear()
+    
+    def analyze_loop(self, func_node: ast.FunctionDef, loop_node: ast.AST):
+        LOGGER.debug("Analyzing loop at line %d in function %s", loop_node.lineno, func_node.name)
+        for child in ast.walk(loop_node):
+            LOGGER.debug("Analyzing node of type: %s at line %d", type(child).__name__, getattr(child, 'lineno', None))
+            if isinstance(child, ast.Call) and isinstance(child.func, ast.Attribute):
+                model, relation_manager, method = self.split_chain(child)
+                
+                LOGGER.info("Checking call to %s on model: %s, relation manager: %s", method, model, relation_manager)
+                if method in self.QUERYSET_METHODS and not self.is_query_optimized(relation_manager):
+                    LOGGER.info("Potential N+1 query found for model: %s, relation manager: %s, method: %s",
+                                model, relation_manager, method)
+                    self.add_issue(func_node, loop_node, child)
+                else:
+                    LOGGER.debug("Queryset %s is optimized", relation_manager)
+            elif self.is_potential_n1_query(child):
+                LOGGER.info("Potential N+1 query found at line %d in loop at line %d", child.lineno, loop_node.lineno)
+                self.add_issue(func_node, loop_node, child)
+            else:
+                LOGGER.debug("Node at line %d is not an N+1 query", getattr(child, 'lineno', None))
 
     def find_optimized_querysets(self, node: ast.AST):
         for child in ast.walk(node):
             if isinstance(child, ast.Call) and isinstance(child.func, ast.Attribute):
                 if child.func.attr in ['select_related', 'prefetch_related']:
                     queryset_name = self.get_queryset_name(child)
-                    self.optimized_querysets.add(queryset_name.lower())
-                    LOGGER.debug("Found optimized queryset: %s", queryset_name)
+                    args = [arg.s for arg in child.args if isinstance(arg, ast.Constant)]
+                    for arg in args:
+                        # Split nested fields and add each level to optimized_querysets
+                        fields = arg.split('__')
+                        for i in range(len(fields)):
+                            field_path = '__'.join(fields[:i + 1])
+                            self.optimized_querysets.add(field_path.lower())
+                    LOGGER.debug("Found optimized queryset: %s with nested fields: %s", queryset_name, args)
 
-            if isinstance(child, ast.Call) and isinstance(child.func, ast.Attribute):
                 method_name = child.func.attr
                 if method_name in self.QUERYSET_METHODS:
                     queryset_name = self.get_queryset_name(child)
@@ -60,78 +88,85 @@ class SimplifiedN1Detector:
         LOGGER.debug("Found %d loops", len(loops))
         return loops
 
-    def analyze_loop(self, func_node: ast.FunctionDef, loop_node: ast.AST):
-        LOGGER.debug("Analyzing loop at line %d in function %s", loop_node.lineno, func_node.name)
-        for child in ast.walk(loop_node):
-            if self.is_potential_n1_query(child):
-                LOGGER.info("Potential N+1 query found at line %d", child.lineno)
-                self.add_issue(func_node, loop_node, child)
-
     def is_potential_n1_query(self, node: ast.AST) -> bool:
-        if isinstance(node, ast.Call):
-            return self.check_call(node)
-        elif isinstance(node, ast.Attribute):
+        if isinstance(node, ast.Attribute):
             chain = self.get_attribute_chain(node)
-            if len(chain) > 2:
-                queryset_name = chain[0]
-                is_optimized = queryset_name in self.optimized_querysets
-                LOGGER.info(f"Is queryset {queryset_name} optimized: {str(self.optimized_querysets)}? {is_optimized}")
-                LOGGER.debug("Checking attribute chain %s. Optimized: %s", '.'.join(chain), is_optimized)
-                return not is_optimized
+            full_chain = '__'.join(chain)
+            
+            LOGGER.debug("Checking full attribute chain: %s in node at line %d", full_chain, node.lineno)
+            
+            # Check if any part of the chain is optimized
+            for i in range(len(chain)):
+                sub_chain = '__'.join(chain[:i + 1])
+                # If any part of the chain is optimized, we skip the detection
+                LOGGER.info("Checking sub-chain: %s", sub_chain)
+                if chain[i] in self.optimized_querysets:
+                    LOGGER.debug("Sub-chain %s found as optimized, skipping N+1 detection", chain[i])
+                    return False
+            
+            LOGGER.info("N+1 query detected with full queryset chain: %s at line %d", full_chain, node.lineno)
+            return True
         return False
 
     def check_call(self, node: ast.Call) -> bool:
         if isinstance(node.func, ast.Attribute):
             method_name = node.func.attr
 
-            # Detects select_related or prefetch_related calls
             if method_name in self.QUERYSET_METHODS:
                 queryset_name = self.get_queryset_name(node)
                 is_optimized = queryset_name in self.optimized_querysets
-                LOGGER.debug("Checking call to %s. Optimized: %s", method_name, is_optimized)
+                LOGGER.debug("Checking call to %s on queryset %s. Optimized: %s", method_name, queryset_name, is_optimized)
                 return not is_optimized
-            
-            # Additional logic to check related object manager accesses (e.g., order.orderitem_set.all())
-            if method_name == 'all':
-                # Check if we're dealing with a related object manager (e.g., orderitem_set)
-                base_name = self.get_queryset_name(node.func.value)
-                if base_name and base_name in self.optimized_querysets:
-                    LOGGER.debug("Found optimized related manager: %s", base_name)
-                    return False 
 
-            # Check for nested calls
-            return self.check_call(node.func.value) if isinstance(node.func.value, ast.Call) else False
+            # If encountering 'all' method, check if related manager has been optimized
+            if method_name == 'all':
+                base_name = self.get_queryset_name(node.func.value)
+                if base_name in self.optimized_querysets:
+                    LOGGER.debug("Found optimized related manager: %s", base_name)
+                    return False
+
+            # Recursively check for nested calls in case of chaining
+            if isinstance(node.func.value, ast.Call):
+                return self.check_call(node.func.value)
+        
         elif isinstance(node.func, ast.Name):
             return node.func.id in list(QUERY_METHODS) + list(WRITE_METHODS)
+        
         return False
 
     def add_issue(self, func_node: ast.FunctionDef, loop_node: ast.AST, query_node: ast.AST):
-        source_segment = ast.get_source_segment(self.source_code, query_node)
-        queryset_name = self.get_queryset_name(query_node)
-        is_optimized = queryset_name in self.optimized_querysets
+        if hasattr(query_node, 'lineno'):
+            source_segment = ast.get_source_segment(self.source_code, query_node)
+            queryset_name = self.get_queryset_name(query_node)
+            is_optimized = queryset_name in self.optimized_querysets
+            
+            LOGGER.debug("Adding issue for queryset: %s (optimized: %s) in loop at line %d", 
+                        queryset_name, is_optimized, loop_node.lineno)
+            
+            explanation, suggestion = self.get_explanation_and_suggestion(query_node, is_optimized)
+            
+            start_line, start_col = self.get_start_of_line(loop_node)
+            end_line, end_col = self.get_end_of_line(query_node)
+            
+            issue = {
+                'id': str(uuid.uuid4()),
+                'function_name': func_node.name,
+                'line': loop_node.lineno,
+                'start_line': start_line,
+                'end_line': end_line,
+                'col_offset': start_col,
+                'end_col_offset': end_col,
+                'message': explanation,
+                'problematic_code': source_segment,
+                'is_optimized': is_optimized,
+                'suggestion': suggestion,
+            }
+
+            self.issues.append(issue)
+            LOGGER.info("Issue added: %s", issue)
+        else:
+            LOGGER.warning("Skipped node without line number %s", query_node)
         
-        explanation, suggestion = self.get_explanation_and_suggestion(query_node, is_optimized)
-
-        start_line, start_col = self.get_start_of_line(loop_node)
-        end_line, end_col = self.get_end_of_line(query_node)
-
-        issue = {
-            'id': str(uuid.uuid4()),
-            'function_name': func_node.name,
-            'line': loop_node.lineno,
-            'start_line': start_line,
-            'end_line': end_line,
-            'col_offset': start_col,
-            'end_col_offset': end_col,
-            'message': explanation,
-            'problematic_code': source_segment,
-            'is_optimized': is_optimized,
-            'suggestion': suggestion,
-        }
-
-        self.issues.append(issue)
-        LOGGER.info("Added issue: %s", issue)
-    
     def get_attribute_chain(self, node: ast.AST) -> List[str]:
         chain = []
         while isinstance(node, ast.Attribute):
@@ -142,13 +177,24 @@ class SimplifiedN1Detector:
         return list(reversed(chain))
 
     def get_queryset_name(self, node: ast.AST) -> str:
-        if isinstance(node, ast.Call):
-            return self.get_queryset_name(node.func.value)
-        elif isinstance(node, ast.Attribute):
-            return self.get_queryset_name(node.value)
-        elif isinstance(node, ast.Name):
-            return node.id
-        return ""
+        chain = []
+        current_node = node
+
+        while isinstance(current_node, (ast.Attribute, ast.Call)):
+            if isinstance(current_node, ast.Call):
+                current_node = current_node.func
+            
+            if isinstance(current_node, ast.Attribute):
+                chain.append(current_node.attr)
+                current_node = current_node.value
+
+        if isinstance(current_node, ast.Name):
+            chain.append(current_node.id)
+
+        chain.reverse()
+        queryset_name = '.'.join(chain)
+
+        return queryset_name
 
     def get_explanation_and_suggestion(self, node: ast.AST, is_optimized: bool) -> Tuple[str, str]:
         LOGGER.debug("Generating explanation and suggestion for node at line %d", node.lineno)
@@ -220,3 +266,39 @@ class SimplifiedN1Detector:
         end_col = len(self.source_code.splitlines()[line - 1].rstrip())
         LOGGER.debug("End of line %d: col %d", line, end_col)
         return line, end_col
+    
+    def split_chain(self, node: ast.AST) -> Tuple[str, str, str]:
+        """
+        Splits the chain into three components: model, relation manager, and method.
+        """
+        chain = []
+        current_node = node
+        LOGGER.info("Splitting chain for node: %s", node)
+
+        while isinstance(current_node, (ast.Attribute, ast.Call)):
+            if isinstance(current_node, ast.Call):
+                method = current_node.func.attr if isinstance(current_node.func, ast.Attribute) else None
+                current_node = current_node.func
+            elif isinstance(current_node, ast.Attribute):
+                chain.append(current_node.attr)
+                current_node = current_node.value
+        
+        # The last remaining node should be the model or instance
+        if isinstance(current_node, ast.Name):
+            model = current_node.id
+            chain.reverse()
+            relation_manager = chain[0] if len(chain) > 0 else None
+            method = chain[-1] if len(chain) > 1 else None
+            return model, relation_manager, method
+
+        return None, None, None
+
+    def is_query_optimized(self, relation_manager: str) -> bool:
+        """
+        Checks if the relation manager is optimized.
+        """
+        LOGGER.info(f"Checking if relation manager {relation_manager} is in optimized querysets: {self.optimized_querysets}")
+        if relation_manager in self.optimized_querysets:
+            LOGGER.debug("Relation manager %s is optimized", relation_manager)
+            return True
+        return False

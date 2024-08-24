2b0ddf66e29a56b788b8cb848d8112d37f9badd5
Tristan Deane <tristandeane93@gmail.com>
2024-08-23 18:42:25 -0600

    Remove deprecated nplusone analyzer and services

--- server/bundled/tools/python/nplusone/__init__.py
+++ server/bundled/tools/python/nplusone/__init__.py
@@ -1,4 +1,2 @@
-from .analyzer import NPlusOneAnalyzer
 from .nplusone_analyzer import NPlusOneDetector
 from .scorer import NPlusOneScorer
-from . import services
--- server/bundled/tools/python/nplusone/services/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .ast_visitor import ASTVisitor
-from .issue_reporter import IssueReporter
-from .queryset_tracker import QuerysetTracker
-from .query_analyzer import QueryAnalyzer
-from .llm_service import LLMService

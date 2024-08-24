d12e5fe7806c72e733b2373ddaff44451663a15d
Tristan Deane <tristandeane93@gmail.com>
2024-08-24 15:24:04 -0600

    Add python tests to test script

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 echo "Running Client Tests..."
 export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
@@ -15,8 +14,19 @@ fi
 echo "Running Server Tests..."
 npm --prefix ./server run test
 
+# Check if the server tests passed before running the Python tests
 if [ $? -ne 0 ]; then
   echo "Server tests failed."
+  exit 1
+fi
+
+echo "Running Python Tests..."
+# Navigate to the Python root directory before running the Python tests
+cd "$(pwd)/server/bundled/tools/python"
+python -m unittest discover -s tests
+
+if [ $? -ne 0 ]; then
+  echo "Python tests failed."
   exit 1
 fi
 

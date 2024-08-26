367a12ece50ce49df7c08428f67296031e8d2a05
Tristan Deane <tristandeane93@gmail.com>
2024-08-26 15:56:41 -0600

    Add check for python install and store executable path

---
+++ server/src/utils/checkForPython.ts
@@ -0,0 +1,29 @@
+import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+import * as path from 'path';
+
+export function checkForPythonAndVenv(): { pythonExecutable: string } | null {
+    try {
+        const hostPythonVersion = execSync('python --version || python3 --version', { encoding: 'utf-8' });
+        console.log(`Python detected: ${hostPythonVersion.trim()}`);
+
+		const isWindows = process.platform === 'win32';
+		console.log("Is Windows host: ", isWindows);
+		console.log("Current directory: ", __dirname);
+        
+        const extensionVenvPath = path.join(__dirname, '../bundled/tools/python/.venv');
+		console.log("Extension venv path: ", extensionVenvPath);
+        const pythonExecutable = path.join(extensionVenvPath, isWindows ? 'Scripts' : 'bin', 'python');
+
+        if (existsSync(extensionVenvPath)) {
+            console.log(`Bundled virtual environment found at ${extensionVenvPath}`);
+            return { pythonExecutable };
+        } else {
+            console.error('Bundled virtual environment not found.');
+            return null;
+        }
+    } catch (error) {
+        console.error('Python is not installed or not found in PATH.');
+        return null;
+    }
+}

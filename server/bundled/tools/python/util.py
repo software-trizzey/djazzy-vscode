f9c9c34397adb69e617c4793f55bf481595fd545
Tristan Deane <tristandeane93@gmail.com>
2024-08-26 15:57:31 -0600

    Add asteriod parser and refactor model lookup

--- server/bundled/tools/python/util.py
+++ server/bundled/tools/python/util.py
@@ -1,4 +1,32 @@
 import ast
+import astroid
+
+from log import LOGGER
+
+
+MODEL_NAME_CHAIN = 'django.db.models.base.Model'
+
+def is_django_model_class(node):
+    """
+    Determines if a node is a Django model class by using astroid to infer types.
+    """
+    if not isinstance(node, astroid.ClassDef):
+        return False
+
+    try:
+        for base in node.bases:
+            inferred_bases = base.infer()
+            for inferred in inferred_bases:
+                # Check if the class is a subclass of 'django.db.models.Model'
+                if isinstance(inferred, astroid.ClassDef) and inferred.qname() == MODEL_NAME_CHAIN:
+                    LOGGER.debug(f"Found subclass of django.db.models.Model: {node.name}")
+                    return True
+    except astroid.InferenceError as e:
+        LOGGER.warning(f"Error inferring base class for {node.name}: {e}")
+
+    LOGGER.debug(f"Class {node.name} is not a Django model")
+    return False
+
 
 def serialize_file_data(obj):
     if isinstance(obj, ast.AST):
@@ -9,3 +37,4 @@ def serialize_file_data(obj):
         return {k: serialize_file_data(v) for k, v in obj.items()}
     else:
         return str(obj)
+    

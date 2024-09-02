9cc5ba4ac947e791d06bb3ce37907dcf65b81b5a
Tristan Deane <tristandeane93@gmail.com>
2024-09-02 09:45:50 -0600

    Add sign in and sign out commands to UI. Add sign in command to message

--- client/src/common/constants/messages.ts
+++ client/src/common/constants/messages.ts
@@ -5,6 +5,6 @@ export enum AUTH_MESSAGES  {
     WELCOME_BETA_MESSAGE = "Welcome to Djangoly (Beta)! 👋 By using this extension, you agree to our Terms of Service and Privacy Policy.",
     INVALID_API_KEY = "The API key you entered is invalid. Please try again or request a new API key using the form below.",
     AUTHENTICATION_REQUIRED = "Authentication is required to use Djangoly. Please sign in or request an API key.",
-    SIGN_OUT = "You have deactivated Djangoly. To reactivate, please restart your IDE. See ya! 👋",
+    SIGN_OUT = "You have deactivated Djangoly. To reactivate, use the 'Djangoly: Sign In' command or restart your IDE. See ya! 👋",
     GITHUB_SIGN_IN = "Sign in to continue using Djangoly. By using this extension you agree to our Terms of Service and Privacy Policy.",
 }

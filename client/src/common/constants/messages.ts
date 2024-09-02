f2c767d361dcfcff463471609116d1e631a4346a
Tristan Deane <tristandeane93@gmail.com>
2024-09-02 09:27:00 -0600

    Ensure users can't bypass the initial auth flow

--- client/src/common/constants/messages.ts
+++ client/src/common/constants/messages.ts
@@ -1,5 +1,6 @@
 
 export enum AUTH_MESSAGES  {
     FREE_API_KEY_PROMPT = "Djangoly is currently in free beta, but a valid API key is required to continue. Don't have an API key? Request one by completing the form below.",
-    INVALID_API_KEY = "The API key you entered is invalid. Please try again or request a new API key using the form below."
+    INVALID_API_KEY = "The API key you entered is invalid. Please try again or request a new API key using the form below.",
+    AUTHENTICATION_REQUIRED = "Authentication is required to use Djangoly. Please sign in or request an API key."
 }
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -24,21 +24,27 @@ import { authenticateUser } from './common/auth/api';
 let client: LanguageClient;
 
 export async function activate(context: vscode.ExtensionContext) {
-	const signIn = "Sign In";
-	const requestAPIKey = "Request API Key";
-    const action = await vscode.window.showInformationMessage(
-        AUTH_MESSAGES.FREE_API_KEY_PROMPT,
-        signIn,
-        requestAPIKey
-    );
+    const signIn = "Sign In";
+    const requestAPIKey = "Request API Key";
+    let isAuthenticated = false;
 
-    if (action === requestAPIKey) {
-        vscode.env.openExternal(vscode.Uri.parse(API_KEY_SIGNUP_URL));
-        await activate(context);
-        return;
-    } else if (action === signIn) {
-        const isAuthenticated = await authenticateUser(context, activate);
-        if (!isAuthenticated) return;
+    while (!isAuthenticated) {
+        const action = await vscode.window.showInformationMessage(
+            AUTH_MESSAGES.FREE_API_KEY_PROMPT,
+            signIn,
+            requestAPIKey
+        );
+
+        if (action === requestAPIKey) {
+            vscode.env.openExternal(vscode.Uri.parse(API_KEY_SIGNUP_URL));
+            await activate(context);
+            return;
+        } else if (action === signIn) {
+            isAuthenticated = await authenticateUser(context, activate);
+            if (!isAuthenticated) {
+                vscode.window.showWarningMessage(AUTH_MESSAGES.AUTHENTICATION_REQUIRED);
+            }
+        }
     }
 
 	const serverModule = context.asAbsolutePath(

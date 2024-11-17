3a36c283c9c01d8fbe42677b720cc889f57459ae
Tristan Deane <tristandeane93@gmail.com>
2024-11-17 12:49:44 -0700

    Enhance error handling in Rollbar integration and add extension ID for better logging context

--- client/src/common/logs.ts
+++ client/src/common/logs.ts
@@ -1,13 +1,32 @@
 import * as vscode from "vscode";
 
 import Rollbar = require("rollbar");
-import { COMMANDS } from './constants';
+import projectPackageJson from "../../package.json";
+import { COMMANDS, DJANGOLY_ID } from './constants';
 
 export const rollbar = new Rollbar({
 	accessToken: "bb31966b163846dcbe5e5d74f30fd9ad",
 	environment: process.env.NODE_ENV === "development" ? "development" : "production",
 	captureUncaught: true,
 	captureUnhandledRejections: true,
+	version: projectPackageJson.version,
+	checkIgnore: (isUncaught, args, item: any) => {
+		if (item.custom && item.custom.vscode && item.custom.vscode.extension) {
+            if (item.custom.vscode.extension !== DJANGOLY_ID) {
+                console.log(`Ignoring error from extension: ${item.custom.vscode.extension}`);
+                return true;
+            }
+        }
+
+		if (item.body.trace_chain && item.body.trace_chain.length > 0) {
+			const exception = item.body.trace_chain[0].exception;
+			if (exception && exception.message === "Canceled") {
+				console.log("Ignoring Canceled: Canceled error", exception);
+				return true;
+			}
+		}
+		return false; // Let all other errors through
+	},
 });
 
 const logger = process.env.NODE_ENV === "development" ? console : rollbar;
